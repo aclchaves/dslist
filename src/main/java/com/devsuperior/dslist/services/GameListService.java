a4639e1914dd2b7ec9885539e1aee6db2b839854
Andre Chaves Lima <aclchaves@gmail.com>
2024-11-12 12:56:40 -0300

    SQL, project, get gams by list

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -6,13 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameListDTO;
-import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
 import com.devsuperior.dslist.repository.GameListRepository;
-import com.devsuperior.dslist.repository.GameRepository;
 
 @Service
 public class GameListService {
